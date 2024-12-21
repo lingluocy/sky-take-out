eb11b2c2246f973d5b7651958d58f2221937175d
lingluocy <lingluocy@gmail.com>
2024-12-21 21:15:07 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -10,6 +12,14 @@ public interface EmployeeMapper {
     // 根据用户名查询员工
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
-
+    
+    //添加员工
     void addEmp(Employee employee);
+
+    //根据id查询员工
+    @Select("select * from employee where id = #{id}")
+     Employee selectById(String id);
+
+    //员工分页查询
+    Page<Employee> page(EmployeePageQueryDTO employeePageQueryDTO);
 }
